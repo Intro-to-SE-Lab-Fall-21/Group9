36cb2937537175fb5e4e18005599af428557e61e
dz183 <69825822+dz183@users.noreply.github.com>
2021-10-21 21:50:02 -0500

    Update test_gp9.py

--- gp9/test_gp9.py
+++ gp9/test_gp9.py
@@ -1,13 +1,29 @@
 import unittest
 
-//username:admin
-//password:admin
+#username:admin & user
+#password:admin & user
 
 class Testgp9(unittest.TestCase):
 
     def SetUp(self):
         self.username = "admin"
         self.password = "admin"
+        self.username2 = "user"
+        self.password2 = "user"
+        self.emailcontent = "I am sending this email to test the system"
+        self.sender = "admin"
+        self.receiver = "user"
+        self.title = "test this message"
 
     def test_login(self):
-        self.assertTrue('admin')
+        self.assertEqual(username, "admin")
+        self.assertEqual(password, "admin")
+
+        self.assertEqual(username2, "user")
+        self.assertEqual(password2, "user")
+
+    def test_emailsent:
+        self.assertTrue
+
+    if __name__ == '__main__':
+        unittest.main()
